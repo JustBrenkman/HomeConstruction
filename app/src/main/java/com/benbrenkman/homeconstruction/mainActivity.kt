162807dc0017b2126e8e6769cfb8c7cc01dab854
JustBrenkman <JustBrenkman@gmail.com>
2018-06-29 09:58:25 -0600

    Updated the color on the cards, changed it to white

--- app/src/main/java/com/benbrenkman/homeconstruction/mainActivity.kt
+++ app/src/main/java/com/benbrenkman/homeconstruction/mainActivity.kt
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.content_main.*
 
