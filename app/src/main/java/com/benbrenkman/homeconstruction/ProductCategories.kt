e2fa9ea7803c0ba7c0e10b8b4d2bce09dd34d6ba
JustBrenkman <JustBrenkman@gmail.com>
2018-07-09 11:37:22 -0600

    Started working on the fragment viewpager. Needs a lot of work.

--- app/src/main/java/com/benbrenkman/homeconstruction/ProductCategories.kt
+++ app/src/main/java/com/benbrenkman/homeconstruction/ProductCategories.kt
@@ -1,14 +1,25 @@
 package com.benbrenkman.homeconstruction
 
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.text.Layout
+import android.view.LayoutInflater
 import android.view.View
 
 import kotlinx.android.synthetic.main.activity_product_categories.*
+import kotlinx.android.synthetic.main.fragment_product_categories.*
 
 class ProductCategories : AppCompatActivity() {
+
+    private lateinit var mPageAdapter: ContentFragmentPageAdapter
+    private lateinit var mViewPager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,17 +28,53 @@ class ProductCategories : AppCompatActivity() {
 
         // Set the tab layout listeners
         category_tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
+            override fun onTabSelected(tab: TabLayout.Tab) {
                 supportActionBar?.setTitle(tab?.text)
+                mViewPager.currentItem = tab.position
             }
 
             override fun onTabReselected(tab: TabLayout.Tab?) {
-                supportActionBar?.setTitle("None")
+                //supportActionBar?.setTitle("None")
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {
-                supportActionBar?.setTitle("Other")
+                //supportActionBar?.setTitle("Other")
             }
         })
+
+        mPageAdapter = ContentFragmentPageAdapter(supportFragmentManager)
+        mViewPager = findViewById(R.id.pager)
+        mViewPager.adapter = mPageAdapter
+
+        mViewPager.setOnPageChangeListener(
+                object : ViewPager.SimpleOnPageChangeListener() {
+                    override fun onPageSelected(position: Int) {
+                        // When swiping between pages, select the
+                        // corresponding tab.
+                        val tab = category_tabs.getTabAt(position)
+                        tab?.select()
+                    }
+                })
     }
+}
+
+class ContentFragmentPageAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    val mList: List<ProductCategoriesFragment> = listOf<ProductCategoriesFragment>()
+
+    fun init() {
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates
+
+        return mList.get(position)
+    }
+
+    override fun getCount(): Int {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return 5;
+    }
+
 }
--- app/src/main/java/com/benbrenkman/homeconstruction/ProductCategoriesFragment.kt
+++ app/src/main/java/com/benbrenkman/homeconstruction/ProductCategoriesFragment.kt
@@ -1,18 +1,29 @@
 package com.benbrenkman.homeconstruction
 
+import android.content.Context
 import android.support.v4.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_product_categories.*
 
 /**
  * A placeholder fragment containing a simple view.
  */
 class ProductCategoriesFragment : Fragment() {
 
+    val parentLinearLayout = scrolling_content_carp
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
         return inflater.inflate(R.layout.fragment_product_categories, container, false)
+    }
+
+    fun onAddCard(view :View) {
+        val inflater:LayoutInflater = activity.layoutInflater
+        val view = inflater.inflate(R.layout.cardview_layout, null)
+        scrolling_content_carp.addView(view)
     }
 }
